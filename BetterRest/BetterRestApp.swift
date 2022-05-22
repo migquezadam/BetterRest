5cd148814b31994c343590be27f9ce08c3cf07e5
Miguel Quezada <mig.aquezada@gmail.com>
2022-05-21 23:34:38 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Miguel Quezada on 21-05-22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
